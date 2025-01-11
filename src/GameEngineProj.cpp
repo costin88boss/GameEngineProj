16f91c09dc0d14f0d21f44b37a6cb1764d037104
costin88boss <costin211.boss@gmail.com>
2025-01-11 16:25:08 +0200

    init

---
+++ src/GameEngineProj.cpp
@@ -0,0 +1,12 @@
+﻿// GameEngineProj.cpp : Defines the entry point for the application.
+//
+
+#include "GameEngineProj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake.A" << endl;
+	return 0;
+}
