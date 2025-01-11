8bebfa13bfe16b17733e2c719296ac4383d4c42f
costin88boss <costin211.boss@gmail.com>
2025-01-11 23:52:15 +0200

    clang formatting and fix cmakelists

--- src/GameEngineProj.cpp
+++ src/GameEngineProj.cpp
@@ -1,18 +1,14 @@
 ﻿#include "GameEngineProj.h"
+#include "glfw3.h"
+#include <iostream>
 
-int main()
-{
-    
-    //a = 2;
+int main() {
+  glfwInit();
 
-
-    glfwInit();
-
-    
-    auto a = glfwCreateWindow(500, 500, "Cancer", 0, 0);
-    while (!glfwWindowShouldClose(a)) {
-        glfwPollEvents();
-    }
-    std::cout << "It fucking works!?!?!? \n";
-    glfwTerminate();
+  auto a = glfwCreateWindow(500, 500, "Cancer", 0, 0);
+  while (!glfwWindowShouldClose(a)) {
+    glfwPollEvents();
+  }
+  std::cout << "It fucking works!?!?!? \n";
+  glfwTerminate();
 }
--- src/GameEngineProj.h
+++ src/GameEngineProj.h
@@ -1,7 +1,3 @@
 ﻿#pragma once
 
-#include <iostream>
-#include <GL/glew.h>
-#include "glfw3.h"
-
 // TODO: Reference additional headers your program requires here.
