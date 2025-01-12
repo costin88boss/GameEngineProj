3cbdc34cc0d16ca9897ccac3295efaf4a74281bc
Pixels <pixels671@protonmail.com>
2025-01-12 18:25:48 +0300

    Legacy OpenGL triangle

--- src/GameEngineProj.cpp
+++ src/GameEngineProj.cpp
@@ -1,14 +1,35 @@
-﻿#include "GameEngineProj.h"
-#include "glfw3.h"
-#include <iostream>
+﻿#include <GL/glew.h>
+#include <glfw3.h>
+#include <stdexcept>
 
-int main() {
-  glfwInit();
+int main()
+{
+    if (!glfwInit())
+        throw std::runtime_error("FATAL: Failed to initialize GLFW3");
 
-  auto a = glfwCreateWindow(500, 500, "Cancer", 0, 0);
-  while (!glfwWindowShouldClose(a)) {
-    glfwPollEvents();
-  }
-  std::cout << "It fucking works!?!?!? \n";
-  glfwTerminate();
+    GLFWwindow *window = glfwCreateWindow(720, 480, "Hello World", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        throw std::runtime_error("FATAL: Failed to create GLFW3 window");
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Yes, I know this is legacy OpenGL, it's just for testing.
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.3f, 0.6f, 0.6f);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glVertex3f(0.5f, -0.5f, 0.0f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
